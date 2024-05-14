9611d903747abdd96f99ddb871f9e172e1ef1ecd
Siddhi24 <99425436+Siddhi24@users.noreply.github.com>
2024-05-14 19:37:03 +0530

    Add files via upload

---
+++ menu1.php
@@ -0,0 +1,103 @@
+<div id="head"><p>Maharastrian Menu is here!!!</p></div>
+    <hr>
+    <div id="mh">
+    <div class="box">
+        <img src="../websitecollegeranker/images/cb.jpg" alt="">
+        <h2 class="h-secondary center">Chicken Biryani 120/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+            
+    </div>
+
+    <div class="box">
+        <img src="../websitecollegeranker/images/mr.jpg" alt="">
+        <h2 class="h-secondary center">Mutton Rassa 100/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+
+    <div class="box">
+        <img src="../websitecollegeranker/images/rp.jfif" alt="">
+        <h2 class="h-secondary center"> Rice Plate 120/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+    </div>
+
+    <div class="box">
+        <img src="../websitecollegeranker/images/thal.jpg" alt="">
+        <h2 class="h-secondary center">Thalipeeth 45/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+    <div class="box">
+        <img src="../websitecollegeranker/images/bv.jpg" alt="">
+        <h2 class="h-secondary center">Bharla Vanga 80/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+    <div class="box">
+        <img src="../websitecollegeranker/images/sk.jpg" alt="">
+        <h2 class="h-secondary center">Sol Kadhi 40/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+    </div>
+    
+    <div class="box">
+        <img src="../websitecollegeranker/images/zb.jfif" alt="">
+        <h2 class="h-secondary center">Jhunka Bhakari 120/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+    <div class="box">
+        <img src="../websitecollegeranker/images/sk.jpg" alt="">
+        <h2 class="h-secondary center">Sabudana Khichadi 30/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+    <div class="box">
+        <img src="../websitecollegeranker/images/misal.jpg" alt="">
+        <h2 class="h-secondary center">Misal Pav 80/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+
+    <div class="box">
+        <img src="../websitecollegeranker/images/paratha.jfif" alt="">
+        <h2 class="h-secondary center">Paratha 65/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+    </div>
+
+    <div class="box">
+        <img src="../websitecollegeranker/images/pp.jpg" alt="">
+        <h2 class="h-secondary center">Puran Poli 50/-</h2>
+        <br>
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+
+    <div class="box">
+        <img src="../websitecollegeranker/images/sm.jpg" alt="">
+        <h2 class="h-secondary center">Sukka Mutton 65/-</h2>
+        <br> 
+        <button class="btn1">Order Now</button>
+        
+        
+    </div>
+</div>
+</div>
+</section>
